84c5159f8d2e02c5c88f7e8dcb4645dea4d7fc3f
Andyke87 <114744748+Andyke87@users.noreply.github.com>
2023-08-04 14:52:06 +0200

    Add files via upload

---
+++ TicTacToe.Domain/SpelController.cs
@@ -0,0 +1,49 @@
+﻿using TicTacToe.Domain;
+namespace TicTacToe.Domain
+{
+    public class SpelController
+    {
+        private Spel _spel { get; set; }
+
+        const char X = 'X';
+        const char O = 'O';
+
+        public SpelController()
+        {
+            _spel = new Spel();
+        }
+        public void RegistreerSpeler(string naam, char symbool)
+        {
+            if (symbool == X)
+            { _spel.RegistreerSpeler(naam, X);}
+            else
+            { _spel.RegistreerSpeler(naam, O);}
+        }
+        public bool RegistreerZet(int vakje)
+        {
+            if (_spel.RegistreerZet(vakje) == true)
+            { return true; }
+            else
+            { return false; }
+        }
+        public string HeeftSpelerGewonnen()
+        {
+            string naam = _spel.HeeftSpelerGewonnen();
+            if (naam != null)
+            { return naam;}
+            else if (_spel.ControleerIsVeldVolzet() == true)
+            { return "Gelijkspel";}
+            else
+            { return null;}
+        }
+        public Speler HuidigeSpeler()
+        { Speler huidigeSpeler = _spel.HuidigeSpeler();
+            return huidigeSpeler;
+        }
+        public void ToonSpelbord()
+        { 
+            Spelbord _spelbord = new Spelbord();
+            _spelbord.ToonVeld();
+        }
+    }
+}
