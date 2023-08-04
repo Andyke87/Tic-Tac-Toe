28cd097545103aa1cd5e32cc44c9aed2a135d4bd
Andyke87 <114744748+Andyke87@users.noreply.github.com>
2023-08-04 13:01:10 +0200

    Add files via upload

---
+++ StartUp.cs
@@ -0,0 +1,16 @@
+﻿using TicTacToe.Cui;
+using TicTacToe.Domain;
+
+namespace TicTacToe.StartUp
+{
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            SpelController controller = new SpelController();
+            TicTacToeApp cui = new TicTacToeApp();
+            cui.Start();
+        }
+    }
+
+}
